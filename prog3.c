fc044b7cbe1ac5cba6a6f6598707ad5d4c4ebfdb
Marco Bolarinho <marcoandrebolarinho@enta.pt>
2020-01-07 12:07:05 -0100

    Retificacao

--- prog3.c
+++ prog3.c
@@ -11,22 +11,31 @@ int main() {
     printf("Escolha um opcao: ");
     scanf("%d", &escolha);
 
-    printf("\n\nInsira o valor: ");
-    scanf("%lf", &num);
-    printf("\n\n");
 
     switch (escolha)
     {
     case 1: // Conversao de euros para usa dollars
+        printf("\n\nInsira o valor: ");
+        scanf("%lf", &num);
+        printf("\n\n");
         printf("%lf Euros sao %lf USA Dollars", num, num * 0.95);
         break;
     case 2: // Conversao de usa dollars para euros
+        printf("\n\nInsira o valor: ");
+        scanf("%lf", &num);
+        printf("\n\n");
         printf("%lf USA Dollars sao %lf Euros", num, num / 0.95);
         break;
-    case 3:
+    case 3: // Conversao de euros para cad
+        printf("\n\nInsira o valor: ");
+        scanf("%lf", &num);
+        printf("\n\n");
         printf("%lf Euros sao %lf CAD", num, num / 0.69);
         break;
-    case 4:
+    case 4: // Conversao de cad para euros
+        printf("\n\nInsira o valor: ");
+        scanf("%lf", &num);
+        printf("\n\n");
         printf("%lf CAD sao %lf Euros", num, num * 0.69);
         break;    
     default:
